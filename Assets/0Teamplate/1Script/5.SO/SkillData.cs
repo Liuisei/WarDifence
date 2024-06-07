8cd28f900a08e9ea5e744e1ca8d1aa8fa6de79b3
Liuisei <116147262+Liuisei@users.noreply.github.com>
2024-06-07 14:14:53 +0900

    SO Chalactor 　と　skill

---
+++ Assets/0Teamplate/1Script/5.SO/SkillData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SkillDataSO", menuName = "Skill Data")]
+public class SkillData : ScriptableObject
+{
+    [SerializeField] int        _skillID;
+    [SerializeField] GameObject _skillPrefab;
+    [SerializeField] float      _skillSpeed;
+    [SerializeField] int        _skillDamage;
+    [SerializeField] float      _skillLifeTime;
+
+    public int        SkillID       { get { return _skillID; } }
+    public GameObject SkillPrefab   { get { return _skillPrefab; } }
+    public float      SkillSpeed    { get { return _skillSpeed; } }
+    public int        SkillDamage   { get { return _skillDamage; } }
+    public float      SkillLifeTime { get { return _skillLifeTime; } }
+}
